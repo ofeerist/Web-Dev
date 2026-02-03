a72b2761499196ab8e7c6002ce33d5420d61293e
Artyom <artemgorbachev046@gmail.com>
2026-02-03 15:26:31 +0500

    Update styles

--- Lab3/task2/script.js
+++ Lab3/task2/script.js
@@ -7,25 +7,40 @@ function addTask() {
     if (text === "") return;
 
     const li = document.createElement('li');
-    li.innerHTML = `
-        <span class="task-text">${text}</span>
-        <button class="delete-btn">Delete</button>
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    const span = document.createElement('span');
+    span.className = 'task-text';
+    span.textContent = text;
+
+    const delBtn = document.createElement('button');
+    delBtn.className = 'delete-btn';
+    delBtn.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+        </svg>
     `;
 
-    li.querySelector('.task-text').addEventListener('click', function() {
-        this.classList.toggle('completed');
+    checkbox.addEventListener('change', () => {
+        span.classList.toggle('completed', checkbox.checked);
     });
 
-    li.querySelector('.delete-btn').addEventListener('click', function() {
+    delBtn.addEventListener('click', () => {
         li.remove();
     });
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(delBtn);
 
     todoList.appendChild(li);
     input.value = "";
 }
 
 addBtn.addEventListener('click', addTask);
-
 input.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') addTask();
 });
